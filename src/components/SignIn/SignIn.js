3821c02c3d848af65b09eec97f988d499140771e
rainswerld <m.rainrobinson@gmail.com>
2020-11-06 14:08:48 -0700

    Update Sign In component to be hook & updated signin axios parameters

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { withRouter } from 'react-router-dom'
 
 import { signIn } from '../../api/auth'
@@ -7,26 +7,16 @@ import messages from '../AutoDismissAlert/messages'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-class SignIn extends Component {
-  constructor () {
-    super()
+const SignIn = props => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-    this.state = {
-      email: '',
-      password: ''
-    }
-  }
-
-  handleChange = event => this.setState({
-    [event.target.name]: event.target.value
-  })
-
-  onSignIn = event => {
+  const onSignIn = event => {
     event.preventDefault()
 
-    const { msgAlert, history, setUser } = this.props
+    const { msgAlert, history, setUser } = props
 
-    signIn(this.state)
+    signIn(email, password)
       .then(res => setUser(res.data.user))
       .then(() => msgAlert({
         heading: 'Sign In Success',
@@ -35,7 +25,6 @@ class SignIn extends Component {
       }))
       .then(() => history.push('/'))
       .catch(error => {
-        this.setState({ email: '', password: '' })
         msgAlert({
           heading: 'Sign In Failed with error: ' + error.message,
           message: messages.signInFailure,
@@ -44,47 +33,43 @@ class SignIn extends Component {
       })
   }
 
-  render () {
-    const { email, password } = this.state
-
-    return (
-      <div className="row">
-        <div className="col-sm-10 col-md-8 mx-auto mt-5">
-          <h3>Sign In</h3>
-          <Form onSubmit={this.onSignIn}>
-            <Form.Group controlId="email">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                required
-                type="email"
-                name="email"
-                value={email}
-                placeholder="Enter email"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                required
-                name="password"
-                value={password}
-                type="password"
-                placeholder="Password"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Button
-              variant="primary"
-              type="submit"
-            >
-              Submit
-            </Button>
-          </Form>
-        </div>
+  return (
+    <div className="row">
+      <div className="col-sm-10 col-md-8 mx-auto mt-5">
+        <h3>Sign In</h3>
+        <Form onSubmit={onSignIn}>
+          <Form.Group controlId="email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              required
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Enter email"
+              onChange={e => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              required
+              name="password"
+              value={password}
+              type="password"
+              placeholder="Password"
+              onChange={e => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button
+            variant="primary"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Form>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default withRouter(SignIn)
