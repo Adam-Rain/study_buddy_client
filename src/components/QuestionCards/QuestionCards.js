e243dff06d0857d0488c5086fbe46562fa7023d7
adamrturman <adamrturman@gmail.com>
2020-12-09 13:24:16 -0600

    brought in card flipping styling for question cards

--- src/components/QuestionCards/QuestionCards.js
+++ src/components/QuestionCards/QuestionCards.js
@@ -2,16 +2,17 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 // import { Link } from 'react-router-dom'
 import QuestionSet from '../QuestionSet/QuestionSet'
+import styles from './QuestionCards.css'
 
 //  display the question sets as individual cards with a title that opens the individual
 //  challenge view
 const QuestionCards = ({ card, id }) => {
   console.log(card)
   return (
-    <Card key={QuestionSet.id}>
-      <Card.Body style={{ textAlign: 'center' }} variant="top">
-        <Card.Title>{card.question}</Card.Title>
-        <Card.Text>{card.answer}
+    <Card className={styles.maincontainer} key={QuestionSet.id}>
+      <Card.Body className={styles.thecard} style={{ textAlign: 'center' }} variant="top">
+        <Card.Title className={styles.thefront}>{card.question}</Card.Title>
+        <Card.Text className={styles.theback}>{card.answer}
         </Card.Text>
       </Card.Body>
     </Card>
