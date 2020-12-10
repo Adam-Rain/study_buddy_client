470080fd4065b2e51ae748db0ade8028c2af5788
adamrturman <adamrturman@gmail.com>
2020-12-10 11:48:08 -0600

    refactored questioncards to use hooks

--- src/components/QuestionCards/QuestionCards.js
+++ src/components/QuestionCards/QuestionCards.js
@@ -1,43 +1,36 @@
 // import React, { useState } from 'react'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Card from 'react-bootstrap/Card'
 // import { Link } from 'react-router-dom'
 import QuestionSet from '../QuestionSet/QuestionSet'
 import styles from './QuestionCards.css'
 import ReactCardFlip from 'react-card-flip'
 
-class QuestionCards extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      isFlipped: false
-    }
-    this.handleClick = this.handleClick.bind(this)
+const QuestionCards = ({ card }) => {
+  const [isFlipped, setIsFlipped] = useState(false)
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    setIsFlipped(!isFlipped)
   }
-  handleClick (e) {
-    e.preventDefault()
-    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }))
-  }
-  render () {
-    const card = this.props.card
-    return (
-      <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
-        <Card className={styles.maincontainer} key={QuestionSet.id}>
-          <Card.Body className={styles.thecard} variant="top">
-            <Card.Title className={styles.thefront}>{card.question}</Card.Title>
-            <button className={styles.btn} onClick={this.handleClick}>Reveal Answer</button>
-          </Card.Body>
-        </Card>
-        <Card className={styles.maincontainer} key={QuestionSet.id}>
-          <Card.Body className={styles.thecard} variant="top">
-            <Card.Text className={styles.theback}>{card.answer}</Card.Text>
-            <button className={styles.btn} onClick={this.handleClick}>Return to Question</button>
-          </Card.Body>
-        </Card>
-      </ReactCardFlip>
-    )
-  }
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <Card className={styles.maincontainer} key={QuestionSet.id}>
+        <Card.Body className={styles.thecard} variant="top">
+          <Card.Title className={styles.thefront}>{card.question}</Card.Title>
+          <button className={styles.btn} onClick={handleClick}>Reveal Answer</button>
+        </Card.Body>
+      </Card>
+      <Card className={styles.maincontainer} key={QuestionSet.id}>
+        <Card.Body className={styles.thecard} variant="top">
+          <Card.Text className={styles.theback}>{card.answer}</Card.Text>
+          <button className={styles.btn} onClick={handleClick}>Return to Question</button>
+        </Card.Body>
+      </Card>
+    </ReactCardFlip>
+  )
 }
+
 //  display the question sets as individual cards with a title that opens the individual
 //  challenge view
 // const QuestionCards = ({ card, id }) => {
