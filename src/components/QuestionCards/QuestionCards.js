9091b494ec5ce103d99e960f1d8af1887a04bf79
adamrturman <adamrturman@gmail.com>
2020-12-10 11:17:48 -0600

    swapped homemade cards for react-card-flip

--- src/components/QuestionCards/QuestionCards.js
+++ src/components/QuestionCards/QuestionCards.js
@@ -1,28 +1,63 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+import React, { Component } from 'react'
 import Card from 'react-bootstrap/Card'
 // import { Link } from 'react-router-dom'
 import QuestionSet from '../QuestionSet/QuestionSet'
 import styles from './QuestionCards.css'
+import ReactCardFlip from 'react-card-flip'
 
+class QuestionCards extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isFlipped: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick (e) {
+    e.preventDefault()
+    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }))
+  }
+  render () {
+    const card = this.props.card
+    return (
+      <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+        <Card className={styles.maincontainer} key={QuestionSet.id}>
+          <Card.Body className={styles.thecard} variant="top">
+            <Card.Title className={styles.thefront}>{card.question}</Card.Title>
+            <button className={styles.btn} onClick={this.handleClick}>Click to flip</button>
+          </Card.Body>
+        </Card>
+        <Card className={styles.maincontainer} key={QuestionSet.id}>
+          <Card.Body className={styles.thecard} variant="top">
+            <Card.Text className={styles.theback}>{card.answer}</Card.Text>
+            <button className={styles.btn} onClick={this.handleClick}>Click to flip</button>
+          </Card.Body>
+        </Card>
+      </ReactCardFlip>
+    )
+  }
+}
 //  display the question sets as individual cards with a title that opens the individual
 //  challenge view
-const QuestionCards = ({ card, id }) => {
-  console.log(card)
+// const QuestionCards = ({ card, id }) => {
+//   console.log(card)
+//
+//   const [flipped, setFlipped] = useState(false)
+//
+//   const handleClick = event => {
+//     event.preventDefault()
+//     setFlipped(!flipped)
+//   }
+//
+//   return (
+//     <Card className={styles.maincontainer} key={QuestionSet.id}>
+//       <Card.Body className={styles.thecard} variant="top">
+//         {flipped ? <Card.Text className={styles.theback}>{card.answer}</Card.Text> : <Card.Title className={styles.thefront}>{card.question}</Card.Title>}
+//         <button className={styles.btn} onClick={handleClick}>{flipped ? 'Return To Question' : 'Reveal Answer'}</button>
+//       </Card.Body>
+//     </Card>
+//   )
+// }
 
-  const [flipped, setFlipped] = useState(false)
-
-  const handleClick = event => {
-    event.preventDefault()
-    setFlipped(!flipped)
-  }
-
-  return (
-    <Card className={styles.maincontainer} key={QuestionSet.id}>
-      <Card.Body className={styles.thecard} variant="top">
-        {flipped ? <Card.Text className={styles.theback}>{card.answer}</Card.Text> : <Card.Title className={styles.thefront}>{card.question}</Card.Title>}
-        <button className={styles.btn} onClick={handleClick}>{flipped ? 'Return To Question' : 'Reveal Answer'}</button>
-      </Card.Body>
-    </Card>
-  )
-}
 export default QuestionCards
