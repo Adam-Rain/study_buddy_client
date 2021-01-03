ed87599d9e24e6d7e61c2e783374b48b8ebdc671
adamrturman <adamrturman@gmail.com>
2021-01-03 13:44:11 -0600

    sign up, sign in, and changepw all in cards

--- src/components/ChangePassword/ChangePassword.js
+++ src/components/ChangePassword/ChangePassword.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { withRouter } from 'react-router-dom'
 import { changePassword } from '../../api/auth'
 import messages from '../AutoDismissAlert/messages'
+import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Footer from '../Footer/Footer'
@@ -33,37 +34,43 @@ const ChangePassword = props => {
   return (
     <div className="row">
       <div className="col-sm-10 col-md-8 mx-auto mt-5">
-        <h3>Change Password</h3>
-        <Form onSubmit={onChangePassword}>
-          <Form.Group controlId="oldPassword">
-            <Form.Label>Old password</Form.Label>
-            <Form.Control
-              required
-              name="oldPassword"
-              value={oldPassword}
-              type="password"
-              placeholder="Old Password"
-              onChange={e => setOldPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="newPassword">
-            <Form.Label>New Password</Form.Label>
-            <Form.Control
-              required
-              name="newPassword"
-              value={newPassword}
-              type="password"
-              placeholder="New Password"
-              onChange={e => setNewPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Button
-            variant="primary"
-            type="submit"
-          >
-            Submit
-          </Button>
-        </Form>
+        <Card>
+          <Card.Header>
+            Change Password
+          </Card.Header>
+          <Card.Body>
+            <Form onSubmit={onChangePassword}>
+              <Form.Group controlId="oldPassword">
+                <Form.Label>Old Password</Form.Label>
+                <Form.Control
+                  required
+                  type="password"
+                  name="oldPassword"
+                  value={oldPassword}
+                  placeholder="Old Password"
+                  onChange={e => setOldPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="newPassword">
+                <Form.Label>New Password</Form.Label>
+                <Form.Control
+                  required
+                  name="password"
+                  value={newPassword}
+                  type="password"
+                  placeholder="New Password"
+                  onChange={e => setNewPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Button
+                variant="primary"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
       </div>
       <Footer/>
     </div>
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -5,6 +5,8 @@ import messages from '../AutoDismissAlert/messages'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Footer from '../Footer/Footer'
+import Card from 'react-bootstrap/Card'
+import Nav from 'react-bootstrap/Nav'
 
 const SignIn = props => {
   const [email, setEmail] = useState('')
@@ -35,7 +37,51 @@ const SignIn = props => {
   return (
     <div className="row">
       <div className="col-sm-10 col-md-8 mx-auto mt-5">
-        <h3>Sign In</h3>
+        <Card>
+          <Card.Header>
+            <Nav variant="tabs" defaultActiveKey="#sign-in">
+              <Nav.Item>
+                <Nav.Link href="#sign-in">Sign In</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#sign-up">Sign Up</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Card.Header>
+          <Card.Body>
+            <Form onSubmit={onSignIn}>
+              <Form.Group controlId="email">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  required
+                  type="email"
+                  name="email"
+                  value={email}
+                  placeholder="Enter email"
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  required
+                  name="password"
+                  value={password}
+                  type="password"
+                  placeholder="Password"
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Button
+                variant="primary"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+        {/* <h3>Sign In</h3>
         <Form onSubmit={onSignIn}>
           <Form.Group controlId="email">
             <Form.Label>Email address</Form.Label>
@@ -65,7 +111,7 @@ const SignIn = props => {
           >
             Submit
           </Button>
-        </Form>
+        </Form> */}
       </div>
       <Footer/>
     </div>
